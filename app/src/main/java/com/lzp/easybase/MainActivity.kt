51a8207ffbe3390b2495e2672922cb8db4dabe97
lizhipeng <lizhipeng@ugirls.com>
2019-06-05 16:58:59 +0800

    Initial commit

---
+++ app/src/main/java/com/lzp/easybase/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.lzp.easybase
+
+import com.lzp.base.activity.BaseNavigationActivity
+
+class MainActivity : BaseNavigationActivity() {
+
+    override fun initView() {
+        setBaseContentView(R.layout.activity_main)
+    }
+
+    override fun initData() {
+    }
+
+}
---
+++ base/src/main/java/com/lzp/base/view/RootLoadingLayout.kt
@@ -0,0 +1,164 @@
+package com.lzp.base.view
+
+import android.content.Context
+import android.support.annotation.LayoutRes
+import android.util.AttributeSet
+import android.view.View
+import android.widget.FrameLayout
+import com.lzp.base.R
+
+/**
+ * Created by li.zhipeng on 2019-06-05.
+ *
+ *      自定义View，具有加载中/加载失败/内容为空等状态，
+ *      适合使用在需要发起网络请求，并显示对应状态提示的场景
+ */
+class RootLoadingLayout @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : FrameLayout(context, attrs, defStyleAttr) {
+
+    private val loadingViewId: Int
+
+    private val loadingEmptyViewId: Int
+
+    private val loadingErrorViewId: Int
+
+    /**
+     * 加载中的布局
+     * */
+    private lateinit var loadingView: View
+
+    /**
+     * 加载为空的布局
+     * */
+    private lateinit var loadingEmptyView: View
+
+    /**
+     * 加载失败的布局
+     * */
+    private lateinit var loadingErrorView: View
+
+    /**
+     * 内部布局
+     * */
+    private lateinit var contentView: View
+
+    var onRootLoadingLayoutListener: OnRootLoadingLayoutListener? = null
+        private set
+
+    init {
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.RootLoadingLayout)
+        // 获取自定义的布局
+        loadingViewId =
+            typedArray.getResourceId(R.styleable.RootLoadingLayout_loadingViewId, R.layout.root_layout_loading)
+        loadingEmptyViewId =
+            typedArray.getResourceId(R.styleable.RootLoadingLayout_loadingEmptyViewId, R.layout.root_layout_empty)
+        loadingErrorViewId =
+            typedArray.getResourceId(R.styleable.RootLoadingLayout_loadingErrorViewId, R.layout.root_layout_load_error)
+
+        typedArray.recycle()
+
+    }
+
+    /**
+     * 设置内容布局
+     * */
+    fun setContentView(@LayoutRes id: Int) {
+        contentView = View.inflate(context, id, this)
+    }
+
+    /**
+     * 设置内容布局
+     * */
+    fun setContentView(contentView: View) {
+        addView(contentView)
+    }
+
+    fun getContentView() = contentView
+
+    fun showLoading() {
+        if (!this::loadingView.isInitialized) {
+            loadingView = View.inflate(context, loadingViewId, this)
+            loadingView.setOnClickListener{
+                onRootLoadingLayoutListener?.onLoadingViewClick()
+            }
+        }
+        loadingView.visibility = View.VISIBLE
+
+        if (this::loadingEmptyView.isInitialized) {
+            loadingEmptyView.visibility = View.GONE
+        }
+
+        if (this::loadingErrorView.isInitialized) {
+            loadingErrorView.visibility = View.GONE
+        }
+
+        if (this::contentView.isInitialized) {
+            contentView.visibility = View.GONE
+        }
+
+    }
+
+    fun showEmpty() {
+        // 初始化加载为空的布局
+        if (!this::loadingEmptyView.isInitialized) {
+            loadingEmptyView = View.inflate(context, loadingEmptyViewId, this)
+            loadingEmptyView.setOnClickListener{
+                onRootLoadingLayoutListener?.onLoadEmptyViewClick()
+            }
+        }
+        loadingEmptyView.visibility = View.VISIBLE
+
+        if (this::loadingView.isInitialized) {
+            loadingView.visibility = View.GONE
+        }
+
+        if (this::loadingErrorView.isInitialized) {
+            loadingErrorView.visibility = View.GONE
+        }
+
+        if (this::contentView.isInitialized) {
+            contentView.visibility = View.GONE
+        }
+
+    }
+
+    fun showLoadError() {
+        // 初始化加载为空的布局
+        if (!this::loadingErrorView.isInitialized) {
+            loadingErrorView = View.inflate(context, loadingErrorViewId, this)
+            loadingErrorView.setOnClickListener{
+                onRootLoadingLayoutListener?.onLoadErrorViewClick()
+            }
+        }
+        loadingErrorView.visibility = View.VISIBLE
+
+        if (this::loadingView.isInitialized) {
+            loadingView.visibility = View.GONE
+        }
+
+        if (this::loadingEmptyView.isInitialized) {
+            loadingEmptyView.visibility = View.GONE
+        }
+
+        if (this::contentView.isInitialized) {
+            contentView.visibility = View.GONE
+        }
+
+    }
+
+    /**
+     * 回调监听，可以在点击对应布局进行重试操作
+     * */
+    interface OnRootLoadingLayoutListener {
+
+        fun onLoadingViewClick()
+
+        fun onLoadErrorViewClick()
+
+        fun onLoadEmptyViewClick()
+
+    }
+
+
+}
